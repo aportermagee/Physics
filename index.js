69a6c673c7d9c52018164a9f937110b6e5b54b40
aportermagee <aportermagee@gmail.com>
2026-01-20 12:20:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'rgb(0, 0, 0)';
-ctx.fillRect(0, 0, 10, 10);
-ctx.fillRect(0, canvas.height - 10, 10, 10);
-ctx.fillRect(canvas.width - 10, 0, 10, 10);
-ctx.fillRect(canvas.width - 10, canvas.height - 10, 10, 10);
